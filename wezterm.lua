932c97364183a058bb94d7dc2a59418182056818
Jack Conner <1jackconner@gmail.com>
2024-04-16 07:35:25 -0700

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,81 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+window_decorations = "RESIZE"
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Frappe"
+
+config.window_frame = {
+	font = wezterm.font({ family = "JetBrains Mono", weight = "Bold" }),
+	active_titlebar_bg = "#232634",
+}
+
+config.colors = {
+	tab_bar = {
+		background = "#626880",
+		inactive_tab_edge = "#626880",
+	},
+}
+--
+-- config.hide_tab_bar_if_only_one_tab = true
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local edge_background = "#303446"
+	local background = "#414559"
+	local foreground = "#51576d"
+
+	if tab.is_active then
+		background = "#8caaee"
+	elseif hover then
+		background = "#8caaee"
+		foreground = "#c6d0f5"
+	end
+
+	local edge_foreground = background
+
+	local title = tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	title = wezterm.truncate_right(title, max_width - 2)
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
+
+-- and finally, return the configuration to wezterm
+return config
